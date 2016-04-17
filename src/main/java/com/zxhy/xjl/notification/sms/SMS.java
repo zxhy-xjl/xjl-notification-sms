7472834695fd9e8aea38ec7ccb1a4ed25c209549
leasonlive <Administrator@PC201408310406>
2016-04-17 15:32:01 +0800

    修改sendMessage在下个版本中将深处，方法为不赞成使用的，请使用send方法

--- src/main/java/com/zxhy/xjl/notification/sms/SMS.java
+++ src/main/java/com/zxhy/xjl/notification/sms/SMS.java
@@ -12,5 +12,13 @@ public interface SMS {
 	 * @return
 	 */
 	public boolean send(String phone, String content);
+	/**
+	 * 这个方法不赞成使用，请使用上面的方法，所有的短信内容都在业务中进行拼装，而不是在这里拼装
+	 * @deprecated 请使用send方法
+	 * @param tel
+	 * @param time
+	 * @param code
+	 * @return
+	 */
 	public int sendMessage(String tel, int time, String code);
 }
