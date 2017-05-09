36923d99c44e1c67262b261ebbed366ca9112ae3
leasonlive <Administrator@192.168.1.103>
2017-05-09 11:26:55 +0800

    增加发送验证码的方法

--- src/main/java/com/xjl/notification/sms/SMS.java
+++ src/main/java/com/xjl/notification/sms/SMS.java
@@ -7,9 +7,16 @@ package com.xjl.notification.sms;
 public interface SMS {
 	/**
 	 * 向某个手机号码发送短信
-	 * @param phone
-	 * @param content
+	 * @param phone 手机号码
+	 * @param content 短信内容
 	 * @return
 	 */
-	public boolean send(String phone, String content);
+	public boolean sendSMS(String phone, String content);
+	/**
+	 * 向某个手机发送验证码
+	 * @param phone 手机号码
+	 * @param verifyCode 验证码
+	 * @return
+	 */
+	public boolean sendVerifyCode(String phone,String verifyCode);
 }
